716147ccb1a73431adc4eba10001c51398ee1cab
igormotta92 <igor.mottta@gmail.com>
2022-02-27 20:54:15 -0300

    refact : remove dir public/news of repository and add dit in git ignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//https://arunmichaeldsouza.com/blog/aliasing-module-paths-in-node-js
+
 import ExtractInfo from '#src/domain/ExtractInfo';
 import ExtractNewsG1 from '#src/extract/ExtractNewsG1';
 
